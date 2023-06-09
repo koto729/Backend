d67b890b83d528bd525987e6218d4a567c5a8050
koto729 <82299926+koto729@users.noreply.github.com>
2023-06-09 09:51:53 +0000

    basic makeup

---
+++ src/DBConnection/conn.js
@@ -0,0 +1,10 @@
+const mongoose =  require("mongoose")
+
+mongoose.connect("mongodb+srv://koto:1234@user.prz7fn7.mongodb.net/?retryWrites=true&w=majority").then(()=>{
+  console.log("connection is setup successfully..")
+}).catch((err))=>{
+  console.log("connection not setup")
+  console.log(err)
+})
+
+("connection is setup sucessfully...")
